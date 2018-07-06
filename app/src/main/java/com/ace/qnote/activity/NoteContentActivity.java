eddcebeac5467c3f20a7c08d8f1ec1c50ece959e
maxiao <190572093@qq.com>
2018-07-06 20:23:01 +0800

    soul

---
+++ app/src/main/java/com/ace/qnote/activity/NoteContentActivity.java
@@ -0,0 +1,45 @@
+package com.ace.qnote.activity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+
+import com.ace.qnote.base.BaseActivity;
+
+public class NoteContentActivity extends BaseActivity{
+
+    @Override
+    public void initParams(Bundle params) {
+
+    }
+
+    @Override
+    public int bindLayout() {
+        return 0;
+    }
+
+    @Override
+    public View bindView() {
+        return null;
+    }
+
+    @Override
+    public void initView(View view) {
+
+    }
+
+    @Override
+    public void setListener() {
+        
+    }
+
+    @Override
+    public void widgetClick(View v) throws Exception {
+
+    }
+
+    @Override
+    public void doBusiness(Context mContext) {
+
+    }
+}
---
+++ app/src/main/java/com/ace/qnote/base/BaseActivity.java
@@ -0,0 +1,269 @@
+package com.ace.qnote.base;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+public abstract class BaseActivity extends AppCompatActivity implements View.OnClickListener{
+
+    /** 是否是标准状态栏 **/
+    private boolean isStandard = true;
+    /** 是否沉浸状态栏 **/
+    private boolean isSetStatusBar = false;
+    /** 是否允许全屏 **/
+    private boolean mAllowFullScreen = false;
+    /** 是否禁止旋转屏幕 **/
+    private boolean isAllowScreenRoate = false;
+    /** 当前Activity渲染的视图View **/
+    private View mContextView = null;
+    /** 日志输出标志 **/
+    protected final String TAG = this.getClass().getSimpleName();
+
+    public Bundle savedInstanceState;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.savedInstanceState = savedInstanceState;
+        Log.d(TAG, "BaseActivity-->onCreate()");
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        Bundle bundle = getIntent().getExtras();
+        initParams(bundle);
+        View mView = bindView();
+        if (null == mView) {
+            mContextView = LayoutInflater.from(this)
+                    .inflate(bindLayout(), null);
+        } else {
+            mContextView = mView;
+        }
+        if (mAllowFullScreen) {
+            requestWindowFeature(Window.FEATURE_NO_TITLE);
+        }
+        if (isSetStatusBar) {
+            steepStatusBar();
+        }
+//        if (isStandard) {
+//            setStandard();
+//        }
+        setContentView(mContextView);
+
+
+        if (!isAllowScreenRoate) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
+        initView(mContextView);
+        setListener();
+        //initPush(getApplicationContext());
+        doBusiness(this);
+    }
+
+/*    private void initPush(Context context) {
+
+        PushAgent.getInstance(context).onAppStart();
+
+    }*/
+
+    public View getmContextView() {
+        return  this.mContextView;
+    }
+
+    /**
+     * [沉浸状态栏]
+     */
+    private void steepStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // 透明状态栏
+            getWindow().addFlags(
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            // 透明导航栏
+//          getWindow().addFlags(
+//                  WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+    }
+
+    /**
+     * [初始化参数]
+     *a
+     * @param params
+     */
+    public abstract void initParams(Bundle params);
+    /**
+     * [绑定布局]
+     *
+     * @return
+     */
+    public abstract int bindLayout();
+
+    /**
+     * [绑定视图]
+     *
+     * @return
+     */
+    public abstract View bindView();
+
+
+
+    /**
+     * [初始化控件]
+     *
+     * @param view
+     */
+    public abstract void initView(final View view);
+
+
+    /**
+     * [设置监听]
+     */
+    public abstract void setListener();
+
+    /** View点击 **/
+    public abstract void widgetClick(View v) throws Exception;
+
+
+    @Override
+    public void onClick(View v) {
+        try {
+            widgetClick(v);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * [业务操作]
+     *
+     * @param mContext
+     */
+    public abstract void doBusiness(Context mContext);
+
+    /**
+     * [页面跳转]
+     *
+     * @param clz
+     */
+    public void startActivity(Class<?> clz) {
+        startActivity(new Intent(BaseActivity.this,clz));
+    }
+
+    /**
+     * [携带数据的页面跳转]
+     *
+     * @param clz
+     * @param bundle
+     */
+    public void startActivity(Class<?> clz, Bundle bundle) {
+        Intent intent = new Intent();
+        intent.setClass(this, clz);
+        if (bundle != null) {
+            intent.putExtras(bundle);
+        }
+        startActivity(intent);
+    }
+
+    /**
+     * [含有Bundle通过Class打开编辑界面]
+     *
+     * @param cls
+     * @param bundle
+     * @param requestCode
+     */
+
+    public void startActivityForResult(Class<?> cls, Bundle bundle,
+                                       int requestCode) {
+        Intent intent = new Intent();
+        intent.setClass(this, cls);
+        if (bundle != null) {
+            intent.putExtras(bundle);
+        }
+        startActivityForResult(intent, requestCode);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy()");
+    }
+
+    /**
+     * [简化Toast]
+     * @param msg
+     */
+    protected void showToast(String msg) {
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * [是否允许全屏]
+     *
+     * @param allowFullScreen
+     */
+    public void setAllowFullScreen(boolean allowFullScreen) {
+        this.mAllowFullScreen = allowFullScreen;
+    }
+
+    /**
+     * [是否设置沉浸状态栏]
+     *
+     * @param isSetStatusBar
+     */
+    public void setSteepStatusBar(boolean isSetStatusBar) {
+        this.isSetStatusBar = isSetStatusBar;
+    }
+
+    /**
+     * [是否允许屏幕旋转]
+     *
+     * @param isAllowScreenRoate
+     */
+    public void setScreenRoate(boolean isAllowScreenRoate) {
+        this.isAllowScreenRoate = isAllowScreenRoate;
+    }
+
+    public void setStandard(boolean standard) {
+        isStandard = standard;
+    }
+
+    public Bundle getSavedInstanceState() {
+        return savedInstanceState;
+    }
+}
