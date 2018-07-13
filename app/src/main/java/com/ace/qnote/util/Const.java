12fd0099510c85636fa3828e48e6ed97c5c3c24a
miraclema(马骁) <miraclema@tencent.com>
2018-07-13 19:17:38 +0800

    完善了编辑界面

--- app/src/main/java/com/ace/qnote/util/Const.java
+++ app/src/main/java/com/ace/qnote/util/Const.java
@@ -2,4 +2,7 @@ package com.ace.qnote.util;
 
 public class Const {
     public static final int CHOOSE_UNIVERISTY = 1001;
+
+    public static final int CONTENT_TYPE_IMG = 2000;
+    public static final int CONTENT_TYPE_TEXT = 2001;
 }
